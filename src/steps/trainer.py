4de5be2d907c72ed6410e4ba970938f1c7258abb
Hamza Tahir <hamza@maiot.io>
2022-01-26 14:25:10 +0100

    Latest checkpoint

--- src/steps/trainer.py
+++ src/steps/trainer.py
@@ -19,7 +19,7 @@ def random_forest_trainer(train_df_x: pd.DataFrame, train_df_y: pd.DataFrame,
                           eval_df_x: pd.DataFrame, eval_df_y: pd.DataFrame,
                           config: RandomForestTrainerConfig) -> RegressorMixin:
 
-    # mlflow.sklearn.autolog()
+    mlflow.sklearn.autolog()
     clf = RandomForestRegressor(max_depth=config.max_depth)
     clf.fit(train_df_x, np.squeeze(train_df_y.values.T))
     eval_score = clf.score(eval_df_x, np.squeeze(eval_df_y.values.T))
